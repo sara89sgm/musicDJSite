d677c13ade8ed275bd8a071d425fa2ed0f3fb60b
hexblaze <he610@doc.ic.ac.uk>
2012-07-11 17:37:14 +0100

    ..

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>S3 tutorial</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+
+<body>
 
 <?php  
 //include the S3 class                
@@ -48,5 +57,6 @@ foreach ($bucket_contents as $file){
     echo "<a href=\"$furl\">$fname</a><br />";  
 }  
 ?>  
-
+</body>
+</html>
 
