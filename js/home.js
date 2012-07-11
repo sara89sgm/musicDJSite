736e408492207e5e626b93369da5926fa808dcdd
hexblaze <he610@doc.ic.ac.uk>
2012-07-11 20:04:26 +0100

    ll

--- js/home.js
+++ js/home.js
@@ -104,3 +104,81 @@ var listSongs = new Array();
 		  }
 		});
 	}
+	
+	
+	
+//====================== METHODS FOR SAVING SESSION
+function setCookie(c_name,value,exdays)
+{
+var exdate=new Date();
+exdate.setDate(exdate.getDate() + exdays);
+var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name)
+{
+var i,x,y,ARRcookies=document.cookie.split(";");
+for (i=0;i<ARRcookies.length;i++)
+{
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x=x.replace(/^\s+|\s+$/g,"");
+  if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
+
+
+
+
+	function loadRequests(){
+	
+	
+
+}
+
+
+
+
+			
+//======================================================================== Classes:
+
+// this is the object constructed after the database query .. it handles generating it's HTML
+// it also inserts itself into the list and registers it's button to link to the correct reqID
+
+	function RequestTile(id, title, desciption, likes,tags,cover){
+		this.id = id;
+		this.title= title;
+		this.desciption= description;
+		this.likes = likes;
+		this.tags = new Array();
+		this.tags[0]= tags[0];
+		this.tags[1]= tags[1];
+		this.tags[2]= tags[2];
+		this.cover = cover;
+		this.writeHTML = writeHTML();
+		this.Draw = Draw;
+		this.respond();
+		}
+	
+	
+	function writeHTML(){
+		var html="<li>";
+		html+= "<img src="+this.cover+" width="600" height="600" /> <a class='aaa' onclick='likeOrunlike();'></a><a class='bbb' onclick=\"flag();'><a class='ccc' onclick=fuuz(); ></a><h3>"+this.title+"</h3><a class='more'>More</a> <span class='respond'> respond</span>";
+		return html;
+	}
+	
+	
+	function Draw(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
--- js/request.js
+++ js/request.js
@@ -56,67 +56,6 @@ function doRequest(){
 	}
 }
 
-//====================== METHODS FOR SAVING SESSION
-function setCookie(c_name,value,exdays)
-{
-var exdate=new Date();
-exdate.setDate(exdate.getDate() + exdays);
-var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-document.cookie=c_name + "=" + c_value;
-}
-
-function getCookie(c_name)
-{
-var i,x,y,ARRcookies=document.cookie.split(";");
-for (i=0;i<ARRcookies.length;i++)
-{
-  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-  x=x.replace(/^\s+|\s+$/g,"");
-  if (x==c_name)
-    {
-    return unescape(y);
-    }
-  }
-}
-
-
-
-
-			
-//======================================================================== Classes:
-
-// this is the object constructed after the database query .. it handles generating it's HTML
-// it also inserts itself into the list and registers it's button to link to the correct reqID
-
-	function RequestTile(id, title, desciption, likes,tags,cover){
-		this.id = id;
-		this.title= title;
-		this.desciption= description;
-		this.likes = likes;
-		this.tags = new Array();
-		this.tags[0]= tags[0];
-		this.tags[1]= tags[1];
-		this.tags[2]= tags[2];
-		this.cover = cover;
-		this.writeHTML = writeHTML();
-		this.Draw = Draw;
-		}
-	
-	
-	function writeHTML(){
-		var html="<li>";
-		//html+= <this.img; 
-		
-			
-	}
-	
-	
-	function Draw(){
-		
-	}
-	
-	
 	
 
 
