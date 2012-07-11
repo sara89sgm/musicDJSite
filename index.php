ac54d6d69a1a2e4a2698107f4cbf8d2f7b7ec6a5
Joe Andrews <eoj995@googlemail.com>
2012-07-11 15:47:06 +0100

    S3

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
 				$fileTempName = $_FILES['theFile']['tmp_name'];
 				
 				//create a new bucket
-				$s3->putBucket("yourbucket", S3::ACL_PUBLIC_READ);
+				$s3->putBucket("fuuzik", S3::ACL_PUBLIC_READ);
 				
 				//move the file
-				if ($s3->putObjectFile($fileTempName, "fuuzik", $fileName, S3::ACL_PUBLIC_READ)) {
+				if ($s3->putObjectFile($fileTempName, "http://fuuzik.s3-website-eu-west-1.amazonaws.com/", $fileName, S3::ACL_PUBLIC_READ)) {
 					echo "<strong>We successfully uploaded your file.</strong>";
 				}else{
 					echo "<strong>Something went wrong while uploading your file... sorry.</strong>";
