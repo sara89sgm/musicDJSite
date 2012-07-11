0888e835e866e252e46900718d0fb234e56f9eff
hexblaze <he610@doc.ic.ac.uk>
2012-07-11 11:30:26 +0100

    merge

--- webApp/responsive-html5css3-template/js/request.js
+++ webApp/responsive-html5css3-template/js/request.js
@@ -1,7 +1,6 @@
 var songs = new Object();
 
 
-
 $(document).ready(function() {
 	$("input.spotify_song_search").autocomplete({
         source: function(request, response) {
@@ -55,4 +54,29 @@ function doRequest(){
 		
 	}
 }
+
+function setCookie(c_name,value,exdays)
+{
+var exdate=new Date();
+exdate.setDate(exdate.getDate() + exdays);
+var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name)
+{
+var i,x,y,ARRcookies=document.cookie.split(";");
+for (i=0;i<ARRcookies.length;i++)
+{
+  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x=x.replace(/^\s+|\s+$/g,"");
+  if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
+
+
 
