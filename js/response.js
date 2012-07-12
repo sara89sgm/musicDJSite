dced5e37f6126ccf1113da1e9e6162781a5b9f05
Joe Andrews <eoj995@googlemail.com>
2012-07-12 18:37:43 +0100

    sdfsd

--- js/response.js
+++ js/response.js
@@ -2,22 +2,22 @@ var myvar = "<?php echo $fileName;?>";
 
 function saveresponse(){
  alert("responding");
-var Response = Parse.Object.extend("Response");
-var response = new Response();
-response.set("track1", $("#entry_1").val());
-response.set("track2", $("#entry_2").val());
-response.set("track3", $("#entry_3").val());
-response.set("track4", $("#entry_4").val());
-response.set("track5", $("#entry_5").val());
-response.set("title1", $("#title1").val());
-response.set("description", $("#description").val());
-response.set("genre", $("#genre").val());
-response.set("tag1", $("#tag1").val());
-response.set("tag2", $("#tag2").val());
-response.set("tag3", $("#tag3").val());
-response.set("by", "User");
-response.set("mix", "https://s3-eu-west-1.amazonaws.com/fuuzik/"+myvar);
-response.save(null, {
+var Resonse = Parse.Object.extend("Resonse");
+var resonse = new Resonse();
+resonse.set("track1", $("#entry_1").val());
+resonse.set("track2", $("#entry_2").val());
+resonse.set("track3", $("#entry_3").val());
+resonse.set("track4", $("#entry_4").val());
+resonse.set("track5", $("#entry_5").val());
+resonse.set("title1", $("#title1").val());
+resonse.set("description", $("#description").val());
+resonse.set("genre", $("#genre").val());
+resonse.set("tag1", $("#tag1").val());
+resonse.set("tag2", $("#tag2").val());
+resonse.set("tag3", $("#tag3").val());
+resonse.set("by", "User");
+resonse.set("mix", "https://s3-eu-west-1.amazonaws.com/fuuzik/"+myvar);
+resonse.save(null, {
  
   success: function(object) {
     alert("Done");
