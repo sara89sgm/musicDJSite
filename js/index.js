8774a9f944cdd78d389e6f4f00d850ae9bbdbf32
Joe Andrews <eoj995@googlemail.com>
2012-07-16 22:39:16 +0100

    more

--- js/index.js
+++ js/index.js
@@ -106,7 +106,7 @@ alert(getCookieValue("yourname"));
 	//alert("wHTML feeder called");
 	//var dateago = prettyDate(this.added); // something goes bad here 
 	//alert(dateago);
-			var html ="<li id='"+this.id+"'><img src=" +"img/bep.jpg "+" width='600' height='600' /> <h3> " + this.title + "</h3>"+ '<a id="" class="aaa request-window" href="#request-box"></a><a id="" class="bbb" href="#request-box"></a><a id="" class="ccc" href="page1.html"></a>'+'<div style="clear:right;"></div>'+"<h1 style='{font-style:italic;}'>By:"+ this.by + "</h1>"+'<a class="more" onclick="more(this);">More</a> <span class="respond">respond</span></li>'; // more and respond
+			var html ="<li id='"+this.id+"'><img src=" +"img/bep.jpg "+" width='600' height='600' /> <h3> " + this.title + "</h3>"+ '<a id="" class="request-window aaa" href="#request-box"></a><a id="" class="bbb" href="#request-box"></a><a id="" class="ccc" href="page1.html"></a>'+'<div style="clear:right;"></div>'+"<h1 style='{font-style:italic;}'>By:"+ this.by + "</h1>"+'<a class="more" onclick="more(this);">More</a> <span class="respond">respond</span></li>'; // more and respond
 		//alert("it looks like this:"+html);
 		return html;
 	}
--- js/modalrequest.js
+++ js/modalrequest.js
@@ -39,41 +39,6 @@ $(document).ready(function() {
 	return false;
 	});
 	$("#signedUP").hide();
-	$('section ul li a.aaa').click(function() {
-		
-		alert("pop");
-		
-                //Getting the variable's value from a link 
-		var requestBox = $(this).attr('href');
-
-		//Fade in the Popup
-		$(requestBox).fadeIn(300);
-		
-		//Set the center alignment padding + border see css style
-		var popMargTop = ($(requestBox).height() + 24 ) /2; 
-		var popMargLeft = ($(requestBox).width() + 24 ) / 2; 
-	
-		$(requestBox).css({ 
-			'margin-top' : -popMargTop,
-			'margin-left' : -popMargLeft
-		});
-		
-		// Add the mask to body
-		$('body').append('<div id="mask"></div>');
-		$('#mask').fadeIn(300);
-	
-		
-		return false;
-	});
-	
-	// When clicking on the button close or the mask layer the popup closed
-	$('a.close, #mask').live('click', function() { 
-	  $('#mask , .request-popup').fadeOut(300 , function() {
-		$('#mask').remove();  
-	}); 
-	return false;
-	});
-	$("#signedUP").hide();
 	
 	
 
