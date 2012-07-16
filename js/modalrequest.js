4e0c26b3c73ac21898892beb111929599dffde56
Joe Andrews <eoj995@googlemail.com>
2012-07-16 15:29:28 +0100

    popup forms

---
+++ js/modalrequest.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	
+	
+
+
+	
+	$('nav ul li.a').click(function() {
+		alert("popup");
+		
+		
+		
+                //Getting the variable's value from a link 
+		var loginBox = $(this).attr('href');
+
+		//Fade in the Popup
+		$(loginBox).fadeIn(300);
+		
+		//Set the center alignment padding + border see css style
+		var popMargTop = ($(loginBox).height() + 24) / 2; 
+		var popMargLeft = ($(loginBox).width() + 24) / 2; 
+		
+		$(loginBox).css({ 
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+		
+		// Add the mask to body
+		$('body').append('<div id="mask"></div>');
+		$('#mask').fadeIn(300);
+	
+		
+		return false;
+	});
+	
+	// When clicking on the button close or the mask layer the popup closed
+	$('a.close, #mask').live('click', function() { 
+	  $('#mask , .request-popup').fadeOut(300 , function() {
+		$('#mask').remove();  
+	}); 
+	return false;
+	});
+	$("#signedUP").hide();
+});
