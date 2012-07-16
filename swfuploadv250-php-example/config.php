8671541526e8463807334a2068d0a7d6b61c3731
Joe Andrews <eoj995@googlemail.com>
2012-07-17 00:57:20 +0100

    d

--- swfuploadv250-php-example/config.php
+++ swfuploadv250-php-example/config.php
@@ -3,7 +3,7 @@
   define('AWS_ACCESS_KEY_ID', 'AKIAITXOXYKBQBBYQN6A');
   define('AWS_SECRET_ACCESS_KEY', 'oQ3oHXoVuTF/5t6Xc4tAv2O0/UZUbyzqCqlbe6ks');
   /**/
-  $S3_BUCKET = "fuuzik";
+  $S3_BUCKET = "https://fuuzik.s3.amazonaws.com/";
 
   /* where I go when I am done uploading */
   $SUCCESS_REDIRECT = "http://localhost/musicDJSite/musicDJSite/swfuploadv250-php-example/success.php";
