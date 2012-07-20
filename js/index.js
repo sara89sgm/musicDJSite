7fc662b112ee3558c8c2db7f2a3e71f9458fc0b5
Houssem <houssem@houssem-MS-7519.(none)>
2012-07-20 02:31:27 +0100

    more is looking better

--- js/index.js
+++ js/index.js
@@ -188,7 +188,7 @@ if (currentUser) {
 
 	
 	function mHTML(){
-		var html = "<div id='more' style='clear:both;'> </h1>"+"tags:<div class='tag' >"+this.tags[0]+"</div>"+"<div class='tag' >"+this.tags[1]+"</div>"+"<div class='tag' >"+this.tags[2]+"</div>"+" description:"+this.description+"<ul id='tracklist'><li>"+this.track1+"</li><li>"+this.track2+"</li><li>"+this.track3+"</li><li>"+this.track4+"</li><li>"+this.track5+"</ul></div>"; // do the same for track2 etc ..
+		var html = "<div id='more' style='clear:both;'>"+"<ul id='tags'><li class='tag' >"+this.tags[0]+"</li>"+"<li class='tag' >"+this.tags[1]+"</li>"+"<li class='tag' >"+this.tags[2]+"</li></ul>"+"<ul id='tracklist'><li>"+this.track1+"</li><li>"+this.track2+"</li><li>"+this.track3+"</li><li>"+this.track4+"</li><li>"+this.track5+"</ul>"+"<div id='desc' >"+this.description+"</div></div>"; // do the same for track2 etc ..
 	return html;
 	}
 	
