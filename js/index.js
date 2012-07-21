93ae068ee3bea17ad2a623efcdbbf3c043634692
Joe Andrews <eoj995@googlemail.com>
2012-07-21 01:29:10 +0100

    fixed erge

--- js/index.js
+++ js/index.js
@@ -369,7 +369,11 @@ var html ="<li class='box' id="+this.id+"><img src="+"'"+this.cover+"'"+" width=
 	//alert("wHTML feeder called");
 	//var dateago = prettyDate(this.added); // something goes bad here 
 	//alert(dateago);
-			var resonsehtml ="<li><audio src='"+this.track5+"'preload='none'  controls></audio></li>"; // do the same for track2 etc ..
+			var resonsehtml ="<li class='box' id="+this.id+"><img src="+"'"+this.cover+"'"+" width='600' height='600' /> <h3>" + this.title + "</h3><a id='' class='aaa'></a><a id='' class='bbb'></a><a id='' class='ccc' onclick=\"like('"+this.id+"')\" href='#request-box'></a><div style='clear:right;'></div><h1 style='{font-style:italic;}'>By:"+ this.by + "</h1> <audio src='"+this.track5+"'preload='none'  controls></audio><a class='more' onclick='more(this);'>More</a><span class='respond' onclick=\"respopup('"+this.id+"')\">respond</span></li>";
+			
+			
+			
+			// do the same for track2 etc ..
 		//alert("it looks like this:"+html);
 		return resonsehtml;
   
