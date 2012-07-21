db0358dd815367936785f6963d880ce9b5884fcd
Joe Andrews <eoj995@googlemail.com>
2012-07-21 01:25:43 +0100

    Revert "a"
    This reverts commit b74f306da7368f0576203b6e565050b08f684aee.

--- js/index.js
+++ js/index.js
@@ -339,8 +339,7 @@ var html ="<li class='box' id="+this.id+"><img src="+"'"+this.cover+"'"+" width=
 
 //========== RESponse section:
 
-	
-	function ResonseTile(id, added,by,title, description, cover, tags, likes, track1, track2,track3,track4,track5, mix){
+	function ResonseTile(id, added,by,title, description, likes,tags,cover,track1,track2,track3,track4,track5,mix){
 		// 5 tracks is just quick(not even that quick) hack .. should use array like for tags
 		this.id = id;
 		this.title= title;
@@ -348,48 +347,36 @@ var html ="<li class='box' id="+this.id+"><img src="+"'"+this.cover+"'"+" width=
 		this.added=added;
 		this.description= description;
 		this.likes = likes;
-		this.track1=track1;
-		this.track2=track2;
-		this.track3=track3;
-		this.track4=track4;
-		this.track5=track5;
 		this.tags = new Array();
 		this.tags[0]= tags[0];
 		this.tags[1]= tags[1];
 		this.tags[2]= tags[2];
 		this.cover = cover;
-		this.mix = mix;
+		this.track1=track1;
+		this.track2=track2;
+		this.track3=track3;
+		this.track4=track4;
+		this.track5=track5;
+		this.mix=mix;
 		this.wresonseHTML = wresonseHTML;
-		this.mresonseHTML = mresonseHTML;
 		//this.respond = respond;
-		this.renderresonse = renderresonse;
+		this.render = render;
 		}
-	
-	function more(more){
-		var id = $(more).parent().attr("id"); 
-				if($(more).parent().hasClass("open")){
-						        $(more).parent().animate({"height":125}).removeClass("open");
-							$("#more").remove();
-						        $(more).html("More...");
-						    }else{
-						        $(more).parent().animate({"height":300}).addClass("open");
-							 $(more).html("Less...");
-							 $(more).parent().append(findReq(id).mHTML());			    
-}
-						}
-
-	
-	function mresonseHTML(){
-		var html = "<div id='more' style='clear:both;'>"+"<ul id='tags'><li class='tag' >"+this.tags[0]+"</li>"+"<li class='tag' >"+this.tags[1]+"</li>"+"<li class='tag' >"+this.tags[2]+"</li></ul>"+"<ul id='tracklist'><li>"+this.track1+"</li><li>"+this.track2+"</li><li>"+this.track3+"</li><li>"+this.track4+"</li><li>"+this.track5+"</ul>"+"<div id='desc' >"+this.description+"</div></div>"; // do the same for track2 etc ..
-	return html;
-	}
 	
 	
 	function wresonseHTML() {
-var html ="<li class='box' id="+this.id+"><img src="+"'"+this.cover+"'"+" width='600' height='600' /> <h3>" + this.title + "</h3><a id='' class='aaa'></a><a id='' class='bbb'></a><a id='' class='ccc' onclick=\"like('"+this.id+"')\" href='#request-box'></a><div style='clear:right;'></div><h1 style='{font-style:italic;}'>By:"+ this.by + "</h1><audio src='"+this.mix+"'preload='none'  controls></audio><a class='more' onclick='more(this);'>More</a><span class='respond' onclick=\"respopup('"+this.id+"')\">respond</span></li>";
-		return html;
+	// ditching the onClick stuff on anchor ..
+	//alert("wHTML feeder called");
+	//var dateago = prettyDate(this.added); // something goes bad here 
+	//alert(dateago);
+			var resonsehtml ="<li><audio src='"+this.track5+"'preload='none'  controls></audio></li>"; // do the same for track2 etc ..
+		//alert("it looks like this:"+html);
+		return resonsehtml;
+  
+
 	}
 	
+
 
 //======================================================================== REquest logic tier
 
@@ -398,27 +385,32 @@ var html ="<li class='box' id="+this.id+"><img src="+"'"+this.cover+"'"+" width=
 		var query = new Parse.Query(Resonse);
 		query.equalTo();
 		query.find({
-		success: function(results) {
-		for(i=0 ; i<results.length ; i++){
-		    var tags= new Array();
-		    tags[0]=results[i].get('tag1');
-		    tags[1]=results[i].get('tag2');
-		    tags[2]=results[i].get('tag3');
-		      
-		    var date= new Date(results[i].createdAt);
-		    ResonseList.push(new ResonseTile(results[i].id,date, results[i].get('by'),results[i].get('title1'),results[i].get('description'),results[i].get('cover'),tags,results[i].get('likes'),
-								results[i].get('track1'),
-								results[i].get('track2'),
-								results[i].get('track3'),
-								results[i].get('track4'),
-								results[i].get('track5'),
-								results[i].get('mix')
-								));		
- 
+		success: function(result) {
+		for(i=0 ; i<result.length ; i++){
+			var resonsetags= new Array();
+			resonsetags[0]=result[i].get('tag1');
+		    resonsetags[1]=result[i].get('tag2');
+		    resonsetags[2]=result[i].get('tag3');
+		    
+		    var date= new Date(result[i].get('createdAt'));
+		    ResonseList.push(new ResonseTile(result[i].get('id'),
+										 date,
+										 result[i].get('by'),
+										 result[i].get('title1'),
+										 result[i].get('description'),
+										 result[i].get('likes'),
+										 resonsetags,
+										 result[i].get('track1'),
+										 result[i].get('track2'),
+										 result[i].get('track3'),
+										 result[i].get('track4'),
+										 result[i].get('track5'),
+										 result[i].get('mix')
+										 ));
+										 
 		}
 		 
 		renderresonse();
-		
 	
 		},
 		error: function(error) {
@@ -430,58 +422,29 @@ var html ="<li class='box' id="+this.id+"><img src="+"'"+this.cover+"'"+" width=
 
 	function renderresonse(){
 		ResonseList.sort(dynamicSort(sortBy));
-		var finalHtml ="<ul id='list-req'>";
+		var finalHtml ="<ul>";
 		
 		// 1. Check sort is correct/
 		// 2. more field fills up description and expands
-		for (i = 0 ; i <5  ;i++) //ResonseList.length
+		for (i = 0 ; i < ResonseList.length ;i++)
 		{
 			finalHtml=finalHtml+ResonseList[i].wresonseHTML();		
 		}
-		finalHtml=finalHtml+'</ul><div id="loadmoreajaxloader" style="display:none;"><center><img src="img/ajax-loader.gif" /></center></div>';
+		finalHtml=finalHtml+"</ul>";
 		// fade out effect :
 		$('#rlist').fadeOut("slow", function(){
 		
 		
-   		 var div = $("<div id='rlist'>"+finalHtml+"</div>").hide(); 
+   		var div = $("<div id='rlist'>"+finalHtml+"</div>").hide(); 
   		 $(this).replaceWith(div);
     	 $('#rlist').fadeIn("slow");
-	
-	});
-	$("#rlist").mCustomScrollbar("update");
-	infiniteloaderresonse();
-}
-
-
-	function infiniteloaderresonse(){
-	setInterval(function() {
-    	if ( didScroll ) {
-        	didScroll = false;
-        	// Check your page position and then
-        	// Load in more results
-	
-		 if(respindex < ResonseList.length)
-    		{
-        		$('#loadmoreajaxloader').show(2000 ,function(){
-			
-			var i;			
-			for(i=respindex; (i<ResonseList.length && i< respindex+step);i++){
-			$("#resonseslist ul").append(ResonseList[i].wresonseHTML());			
-			}
-			respindex=i;
-               		$('#loadmoreajaxloader').fadeOut("slow");	
-			});
-            	}
-		else
-            	{
-                $('#loadmoreajaxloader').html("<center>you've reached the end, you can try making your own request or some music :)</center>");
-		$('#loadmoreajaxloader').show();
-  		}
+});
+		
+		//put it on the screen:
+		//$('requestslist).replaceWith(finalHtml);
+		
+		
 	}
-}, 250);
-}
-	
-
 
 
 
